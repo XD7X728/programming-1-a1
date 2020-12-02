ee17d72278498ca2cc3d5c364335f750108bf553
Hilton Lam <hiltonlam195@gmail.com>
2020-12-02 10:26:07 -0800

    Add n == 1 and n == 2 in Fib algo

--- Pratice Project.py
+++ Pratice Project.py
@@ -3,7 +3,19 @@
 # Code to practice using Git
 
 def main():
-    pass
+    """Calculate the nth Fibonacci number"""
+    # 1, 1, 2, 3, 5, 8, 13, 21, 34
+
+    # Ask the user for which fib number they want
+    num = int(input("Which fib number do you want?"))
+
+    # start at num = 1 -> 1
+    #          num = 2 -> 2
+    #          num = 3 -> 2
+    if num == 1 or num == 2:
+        print(f"The {num}st/nd fibonacci number is 1.")
+    else:
+        for i in range(num):
 
 
 if __name__ == "__main__":
